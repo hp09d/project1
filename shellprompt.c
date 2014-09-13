536c68be4fbe89de201cc8db9574ca0a19f6eeec
Hebe <hp09d@my.fsu.edu>
2014-09-13 15:52:59 -0400

    Collect data from shell command and sorts the command

--- shellprompt.c
+++ shellprompt.c
@@ -1,29 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main()
 {
-char buffer[81];
+	char buffer[81];
 
-char *directory;
-char *username;
-char hostname[81];
-char *buf;
+	char *path;
+	char *token;
 
-long size;
+	char *directory;
+	char *username;
+	char hostname[81];
+	char *buf;
 
-//Collect user information and store into variables for prompt
-size = pathconf(".", _PC_PATH_MAX);
-username = getlogin();
-gethostname(hostname, sizeof(hostname));
+	long size;
 
-if ((buf = (char *)malloc((size_t)size)) != NULL)
-    directory = getcwd(buf, (size_t)size);
+	//Collect user information and store into variables for prompt
+	size = pathconf(".", _PC_PATH_MAX);
+	username = getlogin();
+	gethostname(hostname, sizeof(hostname));
 
-printf("%s@%s:%s $ ",username,hostname,directory);
-fgets(buffer, 81, stdin);
-printf("buffer: %s",buffer);
+	if ((buf = (char *)malloc((size_t)size)) != NULL)
+		directory = getcwd(buf, (size_t)size);
 
-return 0;
+	printf("%s@%s:%s $ ",username,hostname,directory);
+	fgets(buffer, 81, stdin);
+	printf("buffer: %s",buffer);
+	
+	token = strtok(buffer, " ");
+	if(strcmp(token,"cd") == 0)
+	{printf("cd!");}
+	else if (strcmp(token,"exit") == 0)
+	{printf("exit!\n");}
+	else if (strcmp(token,"ioacct") == 0)
+	{printf("ioacct!\n");}
+	else
+	{printf("Success!\n");}
+	return 0;
 }
